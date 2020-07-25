cc3c7636a4c6c4e044332877a66f18ef26bbf0cd
nepomn9shih <nepomn9shih@gmail.com>
2020-07-25 13:56:47 +0300

    End of lesson 8

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -11,16 +11,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point (1, 3, '*');
-            p1.Draw();
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point (4, 5, '#');
-            p2.Draw();
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            upLine.Drow();
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            downLine.Drow();
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            leftLine.Drow();
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            rightLine.Drow();
 
-            HorizontalLine hLine = new HorizontalLine(5, 10, 8, '+');
-            hLine.Drow();
-            VerticalLine vLine = new VerticalLine(5, 8, 13, '+');
-            vLine.Drow();
+            Point p = new Point(4, 5, '*');
+            p.Draw();
 
             Console.ReadLine();
         }
--- Snake/VerticalLine.cs
+++ Snake/VerticalLine.cs
@@ -6,25 +6,15 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class VerticalLine
+    class VerticalLine: Figure
     {
-        List<Point> pList;
-
-        public VerticalLine(int x, int yUp, int yDown, char sym)
+        public VerticalLine(int yUp, int yDown, int x, char sym)
         {
             pList = new List<Point>();
             for (int y = yUp; y <= yDown; y++)
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
-            }
-
-        }
-        public void Drow()
-        {
-            foreach (Point p in pList)
-            {
-                p.Draw();
             }
         }
     }
